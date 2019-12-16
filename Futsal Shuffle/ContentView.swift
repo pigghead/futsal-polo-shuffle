ab9a6b958e4165e4c4152f33c937f55b6ff85e49
Justin Armstrong <justinarmstrong@Justins-MacBook-Pro.local>
2019-12-15 20:55:29 -0500

    Initial commit

---
+++ Futsal Shuffle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Futsal Shuffle
+//
+//  Created by Justin Armstrong on 12/15/19.
+//  Copyright © 2019 Justin Armstrong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
